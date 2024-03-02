993d2cb4f441c5ba0e6d9b96c815bd75e3ba65dd
Tomoe-495 <hasnainsiddique495@gmail.com>
2024-03-02 19:51:31 +0500

    some progress

--- JavaFlorist/Controllers/AccountController.cs
+++ JavaFlorist/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JavaFlorist.Models;
 
 namespace JavaFlorist.Controllers
 {
@@ -12,6 +13,26 @@ namespace JavaFlorist.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(CUSTOMER cust)
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(CUSTOMER cust)
+        {
+
+            return RedirectToAction("Login");
         }
     }
 }
