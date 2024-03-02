ee070cef5d8afdf7cf38747e9f8b140dfbe82f41
Tomoe-495 <hasnainsiddique495@gmail.com>
2024-03-02 20:33:39 +0500

    need to re-design the cards in the shop page, YEA!!

--- JavaFlorist/Controllers/HomeController.cs
+++ JavaFlorist/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JavaFlorist.Models;
 
 namespace JavaFlorist.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        private JavaFloristEntities _db = new JavaFloristEntities();
         public ActionResult Index()
         {
             return View();
@@ -17,7 +19,10 @@ namespace JavaFlorist.Controllers
         public ActionResult Shop()
         {
 
-            return View();
+            List<BOUQUET> _bouqs = _db.BOUQUETs.ToList();
+
+
+            return View(_bouqs);
         }
     }
 }
